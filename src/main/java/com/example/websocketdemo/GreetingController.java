e54938a7f33e7addc68409e9674644091d079550
latif <latifirfansyah@gmail.com>
2025-11-14 14:45:54 +0700

    implement WebSocket communication with greeting functionality

---
+++ src/main/java/com/example/websocketdemo/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.websocketdemo;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+}
